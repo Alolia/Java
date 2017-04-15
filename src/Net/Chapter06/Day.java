128837698fdad889b3506d3b35ee1f3e279ee93f
Alolia <479741918@qq.com>
2017-04-15 19:35:43 +0800

    Change-Id: Ibaf51790745aa53e28039079de4df52175f217e8

---
+++ src/Net/Chapter06/Day.java
@@ -0,0 +1,11 @@
+package Net.Chapter06;
+
+public class Day {
+
+	public static void main(String[] args) {
+		int day[]=new int[]{1,2,3,4,5,6,7,8,9,10,11,12};
+		for (int i=0;i<12;i++){
+			System.out.println((i+1)+" month have "+day[i]+" days");
+	}
+	}
+}
