a0431071f6d05a67d145b1b4f8a134ce2db1b1e7
Alolia <479741918@qq.com>
2017-04-15 19:46:09 +0800

    Change-Id: Icf6be8666f85a38760e39f242ffb56d94987af5d

---
+++ src/Net/Chapter05/SuoYin.java
@@ -0,0 +1,12 @@
+package Net.Chapter05;
+
+public class SuoYin {
+
+	public static void main(String[] args) {
+		String a ="ukasdhgaherifgqpfg";
+		int size =a.lastIndexOf("");
+		System.out.println("空字符索引位置是" +a.length());
+		System.out.println("字符串长度是" +a.length());
+	}
+
+}
