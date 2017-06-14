299ec4d087a9e269c1f478354264450aac3be605
Administrator <Administrator@192.168.0.103>
2017-06-15 00:18:06 +0800

    Change-Id: I698026b4512264321ce77ffad1d40ce1ce6559c0

---
+++ src/Net/Chapter18/ThreadLocalTest.java
@@ -0,0 +1,76 @@
+package Net.Chapter18;
+
+public class ThreadLocalTest {
+	
+	private static ThreadLocal<User> userThreadLoacl = new ThreadLocal<User>();
+	
+	public static void main(String[] args) {
+		for(int i=1;i<100;i++){
+			final int index= i;
+			new Thread(new Runnable(){
+				@Override
+				public void run() {
+					User user= new User();
+					user.setName("tester" + index);
+					userThreadLoacl.set(user);
+					a();
+					b();
+					c();
+					d();
+				}
+			}).start();
+		}
+	}
+
+	private static void a(){
+		User currentUser = userThreadLoacl.get();
+		System.out.println(currentUser.getName() + "Called a method...");
+		try{
+			Thread.sleep(10);
+		}catch(InterruptedException e){
+			
+		}
+	}
+	private static void b(){
+		User currentUser = userThreadLoacl.get();
+		System.out.println(currentUser.getName() + "Called b method...");
+		try{
+			Thread.sleep(10);
+		}catch(InterruptedException e){
+			
+		}
+	}
+	private static void c(){
+		User currentUser = userThreadLoacl.get();
+		System.out.println(currentUser.getName() + "Called c method...");
+		try{
+			Thread.sleep(10);
+		}catch(InterruptedException e){
+			
+		}
+	}
+	private static void d(){
+		User currentUser = userThreadLoacl.get();
+		System.out.println(currentUser.getName() + "Called d method...");
+		try{
+			Thread.sleep(10);
+		}catch(InterruptedException e){
+			
+		}
+	}
+}
+
+class User{
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
